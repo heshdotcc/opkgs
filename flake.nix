fd3a9537a579d0e31384094b36349542b54d9b3f
he <yo@hesh.cc>
2024-06-30 03:46:44 -0300

    meta: initial commit

--- flake.nix
+++ flake.nix
@@ -3,9 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, ... }: {
+  outputs = { self, nixpkgs, flake-utils, ... }: {
     overlays = [
       (final: prev: {
         base64 = import ./pkgs/base64.nix { inherit (final) lib; };
@@ -13,6 +14,9 @@
     ];
 
     toBase64 = (import ./pkgs/base64.nix { inherit (nixpkgs.lib); }).toBase64;
+
+    lib = {
+      inherit (flake-utils.lib) eachDefaultSystem;
+    };
   };
 }
-
